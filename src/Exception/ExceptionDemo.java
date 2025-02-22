d2bd1bbac3a6913743889882c371f100e158c9df
Akhtar Rahat <rahatakhter4@gmail.com>
2025-02-22 16:07:11 +0600

    Java explicit finally method to close reader

--- src/Exception/ExceptionDemo.java
+++ src/Exception/ExceptionDemo.java
@@ -2,37 +2,21 @@ package Exception;
 
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 
 public class ExceptionDemo {
     public static  void show() {
-        FileReader reader = null;
-        try {
-            reader = new FileReader("file.txt");
+        try(
+                var reader = new FileReader("file.txt");
+                var writer = new FileWriter("output.txt");
+        ) {
             var value = reader.read();
-            System.out.println("File opened");
-//            new SimpleDateFormat().parse("");
         }
-//        catch (FileNotFoundException ex) {
-//            System.out.println(ex.getMessage());
-//            System.out.println(System.getProperty("user.dir"));
-//        }
         catch (IOException e) {
-            System.out.println(e.getMessage());
+            System.out.println("Could not read data");
         }
-        finally {
-            if(reader != null) {
-                try{
-                    reader.close();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
-        }
-//        catch (ParseException e) {
-//            throw new RuntimeException(e);
-//        }
     }
 }
